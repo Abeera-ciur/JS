cb8762da06aaa40ea49f4d7ac56e94a0db97de7b
Abeera-ciur <cyra.ciur.official@gmail.com>
2026-02-28 21:45:30 +0500

    02_DataTypes

---
+++ 01_Basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS as newer version  ---- better practice 
+
+//alert("Hello")  we r using node js hence gives error. doesnt give error in browser
+
+console.log(3+3) ; console.log(3+3) // need ; f 2 lines of code in single line
+
+// Data Types 
+
+let name = "Dee"        // String, can be in single qoute, double qoutr          
+let age = 18            //digit          
+let isLoggedIn = false  //Boolen      
+null                    // stand alone value
+undefined               // placeholder
+//symbol                //used in figma, react js, used for uniquesness smh.
+
+console.log(typeof "Dee")    // typof gives the like data type of the var
+console.log(typeof age) 
+console.log(typeof null) 
+console.log(typeof undefined) 
+
+
